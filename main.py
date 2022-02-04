5a7ada0862f26475bb860c7ee2a6373a294b9bfe
jenasuman <95709987+jenasuman@users.noreply.github.com>
2022-02-04 17:34:57 +0530

    Create main.py
    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+import datetime as dt
+from sklearn.cluster import DBSCAN
+df = pd.read_json(‘livedata.json’)
+df.head()
+
+def get_infected_names(input_name):
+
+    epsilon = 0.0018288 # a radial distance of 6 feet in kilometers
+    model = DBSCAN(eps=epsilon, min_samples=2, metric='haversine').fit(df[['latitude', 'longitude']])
+    df['cluster'] = model.labels_.tolist()
+
+    input_name_clusters = []
+    for i in range(len(df)):
+        if df['id'][i] == input_name:
+            if df['cluster'][i] in input_name_clusters:
+                pass
+            else:
+                input_name_clusters.append(df['cluster'][i])
+    
+    infected_names = []
+    for cluster in input_name_clusters:
+        if cluster != -1:
+            ids_in_cluster = df.loc[df['cluster'] == cluster, 'id']
+            for i in range(len(ids_in_cluster)):
+                member_id = ids_in_cluster.iloc[i]
+                if (member_id not in infected_names) and (member_id != input_name):
+                    infected_names.append(member_id)
+                else:
+                    pass
+    return infected_names
